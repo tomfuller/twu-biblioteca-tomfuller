531301b0a7fb7837d212258aca51da62e9b353d0
Tom Fuller <tomf95@hotmail.co.uk>
2017-06-07 11:19:24 +0100

    movie with rating tested and functional

---
+++ src/com/twu/biblioteca/Movie.java
@@ -0,0 +1,35 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by tomfuller on 01/06/2017.
+ */
+public class Movie {
+
+    private String name;
+    private int year;
+    private String director;
+    private String rating;
+
+    public Movie(String movieName, int movieYear, String movieDirector, String movieRating) {
+        name = movieName;
+        year = movieYear;
+        director = movieDirector;
+        rating = movieRating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+}
---
+++ test/com/twu/biblioteca/MovieTest.java
@@ -0,0 +1,35 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tomfuller on 01/06/2017.
+ */
+public class MovieTest {
+
+    Movie movie = new Movie("A Knights Tale", 2001, "Brian Helgeland", "8");
+
+    @Test
+    public void getMovieName() throws Exception {
+        assertEquals("A Knights Tale", movie.getName());
+    }
+
+    @Test
+    public void getMovieYear() throws Exception {
+        assertEquals(2001, movie.getYear());
+    }
+
+
+    @Test
+    public void getMovieDirector() throws Exception {
+        assertEquals("Brian Helgeland", movie.getDirector());
+    }
+
+
+    @Test
+    public void getMovieRating() throws Exception {
+        assertEquals("8", movie.getRating());
+    }
+}
